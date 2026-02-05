12c1afd06fb93cc6af1c4b5b45ceafaca90111e8
Katie Linder <Kaitlyn.E.Linder-1@ou.edu>
2026-02-05 15:57:04 -0600

    Complete part 3a

--- Hw1.py
+++ Hw1.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
+import math
 
 #Problem 2
 #Part a
 
 #define data to interpolate
-xdata = np.array([-1, 0, 1, 2, 3, 3.5, 5, 6, 7, 8])
-ydata = np.array([-3, -1, 1, 3, 3, 1, 4, 2, 1, 1])
+p2xdata = np.array([-1, 0, 1, 2, 3, 3.5, 5, 6, 7, 8])
+p2ydata = np.array([-3, -1, 1, 3, 3, 1, 4, 2, 1, 1])
 
 #create the interpolation function
 def piecewise(x, xdata, ydata):
@@ -32,12 +33,12 @@ def piecewise(x, xdata, ydata):
     return y
 
 #create test data
-x = np.linspace(xdata[0], xdata[-1], 100)
-y = piecewise(x, xdata, ydata)
+x = np.linspace(p2xdata[0], p2xdata[-1], 100)
+y = piecewise(x, p2xdata, p2ydata)
 
 #plot the results
-plt.scatter(xdata, ydata, label="data with interpolation")
-plt.plot(xdata, ydata)
+plt.scatter(p2xdata, p2ydata, label="data with interpolation")
+plt.plot(p2xdata, p2ydata)
 plt.scatter(x, y, label="test points", alpha = 0.5)
 plt.xlabel("x")
 plt.ylabel("y")
@@ -50,15 +51,43 @@ plt.show()
 #Part b
 
 #Use the scipy code with variables defined in part a to interpolate
-csp = sp.interpolate.CubicSpline(xdata,ydata)
+csp = sp.interpolate.CubicSpline(p2xdata,p2ydata)
 y_csp = csp(x)
 
 #plot the results against eachother
-plt.scatter(xdata, ydata, label="origional data")
+plt.scatter(p2xdata, p2ydata, label="origional data")
 plt.scatter(x, y_csp, label="cubic spline", alpha = 0.5)
 plt.scatter(x, y, label="piecewise", alpha = 0.5)
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Two ways of Interpolation")
+plt.legend()
+plt.show()
+
+
+
+#Problem 3
+#create the dataset
+def p3eqn(x):
+    return np.sin((math.pi/2)*x) + x/2
+
+p3xdata = np.arange(11)
+p3ydata = p3eqn(p3xdata)
+
+#part a
+#create the interpolation using the two methods
+p3x = np.linspace(p3xdata[0], p3xdata[-1], 100)
+p3y = piecewise(p3x, p3xdata, p3ydata)
+
+p3csp = sp.interpolate.CubicSpline(p3xdata, p3ydata)
+p3y_csp = p3csp(p3x)
+
+#plot the results
+plt.scatter(p3x, p3y_csp, label="cubic spline", alpha = 0.5)
+plt.scatter(p3x, p3y, label="piecewise", alpha = 0.5)
+plt.scatter(p3xdata, p3ydata, label="origional data")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Sin Interpolation")
 plt.legend()
 plt.show()
