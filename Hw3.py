83408c49609ec64a6efac8b8beae51017435758b
Katie Linder <Kaitlyn.E.Linder-1@ou.edu>
2026-02-19 15:24:43 -0600

    Make a RK4 function

--- Hw3.py
+++ Hw3.py
@@ -16,9 +16,9 @@ def euler(f, xi, xf, yi, n):
     h = (xf - xi) / n
     x = xi
     y = yi
-    i = 0
+
+    #create the loop to do the intergration
     while x < xf:
-        i += 1
         y += h * f(x, y)
         x += h
     else:
@@ -26,6 +26,35 @@ def euler(f, xi, xf, yi, n):
 
 
 #test the euler code
-test = euler(p1, 0, 1, 0, 5) 
+testeu = euler(p1, 0, 1, 0, 5) 
 true = np.tan(1)
-print(f'The value from the euler method is {test:.3g} and the true value is {true:.3g}')
+print(f'The value from the euler method is {testeu:.5g} and the true value is {true:.5g}')
+
+
+
+def RK4(f, xi, xf, yi, n):
+    #f is the function to integrate, xi and xf are the end points of integration, yi is the
+    #initial y value and n is the number of steps.
+    
+    #define h (step size), initial x value, y value and iteration
+    h = (xf - xi) / n
+    x = xi
+    y = yi
+
+    #create the loop to do the integration
+    while x < xf:
+        k1 = f(x, y)
+        k2 = f(x + h/2, y + (h / 2) * k1)
+        k3 = f(x + h/2, y + (h / 2) * k2)
+        k4 = f(x + h, y + h * k3)
+
+        y += (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+        x += h
+    else:
+        return y
+    
+
+
+#test the RK code
+testRK = RK4(p1, 0, 1, 0, 5)
+print(f'The value from the RK4 method is {testRK:.5g} and the true value is {true:.5g}')
