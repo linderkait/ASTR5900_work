fd9fbb2893fb8c594f179067572ea34e7b51e528
Katie Linder <Kaitlyn.E.Linder-1@ou.edu>
2026-03-26 11:44:45 -0500

    work on animation

--- Hw4.py
+++ Hw4.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from functools import partial
+
 #Problem 1
 #Part a
 #define a two body code
-def twobody(x, y, vx, vy):
+def twobody(frame, x, y, vx, vy):
     #x and y are the initial position coordinates, vx and vy are initial velocityis, delt is the time step. 
     #A circular orbit is assumed.
 
@@ -40,7 +42,7 @@ def twobody(x, y, vx, vy):
 
     return x, y, vx, vy
 
-def euler(x, y, vx, vy):
+def euler(frame, x, y, vx, vy):
     #x and y are the initial position coordinates, vx and vy are initial velocityis, delt is the time step. 
     #A circular orbit is assumed.
 
@@ -68,21 +70,31 @@ def euler(x, y, vx, vy):
 
 
 #define initial values
-x = 1
-y = 0
-r = np.sqrt(x**2 + y**2)
+x0 = 1
+y0 = 0
+r = np.sqrt(x0**2 + y0**2)
 
 G = 6.674e-11
 M = 1 
 
 v_tot = np.sqrt(G * M / r)    #velocity of orbit
-vx = v_tot * (y / r)    #velocity in the x direction
-vy = v_tot * (x / r)    #velocity in the y directon
+vx0 = v_tot * (y0 / r)    #velocity in the x direction
+vy0 = v_tot * (x0 / r)    #velocity in the y directon
 
 t = 3    #total time to run simulation in years
 delt = 0.05    #timestep 
 n = t/delt    #total number of steps
 
 #make animation
-fig = plt.Figure()
-FuncAnimation(fig)
+fig, ax = plt.subplots()
+
+twobody, = ax.plot([], [])
+
+def init():
+    twobody.set_data([], [], [], [])
+    return twobody
+
+
+two_ani = FuncAnimation(fig, partial(twobody, x = x0, y = y0, vx = vx0, vy = vy0), save_count = int(t/delt), init_func = init)
+two_ani.to_jshtml()
+plt.show()
