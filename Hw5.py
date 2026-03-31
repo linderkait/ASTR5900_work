843bd6f11c8f432b2aafa99e957a5bde79ef8095
Katie Linder <Kaitlyn.E.Linder-1@ou.edu>
2026-03-31 11:44:46 -0500

    Begin problem 1b

---
+++ Hw5.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Problem 1
+#Part b
+def DFT(f, N):
+    #the function takes a function f(x) and the number of steps to use N and outputs the 
+    #Fourier transform F(k)
+
+
+
+def iDFT(F, N):
+    #the function takes a function F(k) and the number of steps N and outputs the 
+    #Fourier transform f(x)
+    
