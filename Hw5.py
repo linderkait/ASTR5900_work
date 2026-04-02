42e200a061252f5b00b061359ca9ee8ee6119ec8
Katie Linder <Kaitlyn.E.Linder-1@ou.edu>
2026-04-02 14:09:05 -0500

    Complete problem 1

--- Hw5.py
+++ Hw5.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
+import time
 
 #Problem 1
 #Part b
@@ -53,8 +55,8 @@ plt.plot(k2, np.abs(T2), label = f'N = {N[1]}')
 plt.plot(k3, np.abs(T3), label = f'N = {N[2]}')
 plt.plot(k3, np.abs(Ttrue), label = 'analytical result')
 plt.title("Fourier Transform")
-plt.xlabel('F(k)')
-plt.ylabel('k')
+plt.ylabel('F(k)')
+plt.xlabel('k')
 plt.legend()
 plt.show()
 
@@ -69,9 +71,8 @@ iT3 = iDFT(T3, N[2])
 x3 = np.linspace(xi, xf, N[2])
 
 #calculate the analytical result
-iFtrue = lambda x: np.exp(-50 * (x - 0.5) ** 2)
 
-iTtrue = iFtrue(x3)
+iTtrue = f(x3)
 
 #Plot the inverses
 plt.plot(x1, iT1, label = f'N = {N[0]}')
@@ -79,7 +80,63 @@ plt.plot(x2, iT2, label = f'N = {N[1]}')
 plt.plot(x3, iT3, label = f'N = {N[2]}')
 plt.plot(x3, iTtrue, label = 'Origional Function')
 plt.title("Inverse Fourier Transform")
-plt.xlabel('f(x)')
-plt.ylabel('x')
+plt.ylabel('f(x)')
+plt.xlabel('x')
 plt.legend()
 plt.show()
+
+
+
+#Part c
+#plot the two functions
+test = scipy.fft.fft(f(x2))
+
+plt.plot (k2, np.abs(test), label = 'scipy code')
+plt.plot(k2, np.abs(T2), label = 'My code', lw = 0.8)
+plt.ylabel('F(k)')
+plt.xlabel('k')
+plt.legend()
+plt.show()
+
+#time the two codes
+#start with FFT
+t_FFT = np.array([])
+j_FFT = np.arange(3, 21)
+N_FFT = 2 ** j_FFT
+
+for i in N_FFT:
+    x = np.linspace(0, 1, i)
+    t_tot = np.array([])
+    for j in range(100):
+        start = time.perf_counter()
+        fft = scipy.fft.fft(f(x))
+        end = time.perf_counter()
+        t = end - start
+        t_tot = np.append(t_tot, t)
+    t_FFT = np.append(t_FFT, np.average(t_tot))
+
+#Now DFT
+t_DFT = np.array([])
+j_DFT = np.arange(3, 11)
+N_DFT = 2 ** j_DFT
+
+for i in N_DFT:
+    x = np.linspace(0, 1, i)
+    t_tot = np.array([])
+    for j in range(100):
+        start = time.perf_counter()
+        dft = DFT(f, i, 0, 1)
+        end = time.perf_counter()
+        t = end - start
+        t_tot = np.append(t_tot, t)
+    t_DFT = np.append(t_DFT, np.average(t_tot))
+
+plt.loglog(N_FFT, t_FFT, label = "FFT")
+plt.loglog(N_DFT, t_DFT, label = "DFT")
+plt.ylabel('t')
+plt.xlabel('N')
+plt.legend()
+plt.show()
+
+
+#Problem 2
